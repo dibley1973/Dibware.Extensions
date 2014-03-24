108b2a5c5f64ce5ce2388b3dc1e78cba464b9643
dibley1973 <dibley1973@yahoo.co.uk>
2014-03-24 04:59:45 +0000

    20140324-0459-DWDT Added String Extensions and tests

---
+++ Dibware.System.Extensions.Tests/System/StringExtensionTests.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dibware.System.Extensions.Tests
+{
+    /// <summary>
+    /// Provides extensions to the string class
+    /// </summary>
+    [TestClass]
+    public class StringExtensionTests
+    {
+        #region HasValue
+
+        [TestMethod]
+        public void Test_HasValue_ReturnsFalse_ForNullString()
+        {
+            // Arrange
+            const String value = null;
+            const Boolean expectedValue = false;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.HasValue());
+        }
+
+        [TestMethod]
+        public void Test_HasValue_ReturnsFalse_ForEmptyString()
+        {
+            // Arrange
+            const String value = "";
+            const Boolean expectedValue = false;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.HasValue());
+        }
+
+        [TestMethod]
+        public void Test_HasValue_Returnstrue_ForFilledString()
+        {
+            // Arrange
+            const String value = "SomeValue";
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.HasValue());
+        }
+
+        [TestMethod]
+        public void Test_HasValue_Returnstrue_ForWhiteSpace()
+        {
+            // Arrange
+            const String value = "  ";
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.HasValue());
+        }
+
+        #endregion
+
+        #region IsNullOrEmpty
+
+        [TestMethod]
+        public void Test_IsNullOrEmpty_ReturnsTrue_ForNullString()
+        {
+            // Arrange
+            const String value = null;
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrEmpty());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrEmpty_ReturnsTrue_ForEmptyString()
+        {
+            // Arrange
+            const String value = "";
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrEmpty());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrEmpty_ReturnsFalse_ForFilledString()
+        {
+            // Arrange
+            const String value = "SomeValue";
+            const Boolean expectedValue = false;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrEmpty());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrEmpty_ReturnsFalse_ForWhiteSpace()
+        {
+            // Arrange
+            const String value = "   ";
+            const Boolean expectedValue = false;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrEmpty());
+        }
+
+        #endregion
+
+        #region IsNullOrWhiteSpace
+
+        [TestMethod]
+        public void Test_IsNullOrWhiteSpace_ReturnsTrue_ForNullString()
+        {
+            // Arrange
+            const String value = null;
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrWhiteSpace_ReturnsTrue_ForEmptyString()
+        {
+            // Arrange
+            const String value = "";
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrWhiteSpace_ReturnsFalse_ForFilledString()
+        {
+            // Arrange
+            const String value = "SomeValue";
+            const Boolean expectedValue = false;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrWhiteSpace_ReturnsFalse_ForWhiteSpace()
+        {
+            // Arrange
+            const String value = "    ";
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrWhiteSpace());
+        }
+
+        #endregion
+
+        #region IsNullOrEmptyOrWhiteSpace
+
+        [TestMethod]
+        public void Test_IsNullOrEmptyOrWhiteSpace_ReturnsTrue_ForNullString()
+        {
+            // Arrange
+            const String value = null;
+            const Boolean expectedValue = true;
+
+            // Act
+
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrEmptyOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrEmptyOrWhiteSpace_ReturnsTrue_ForEmptyString()
+        {
+            // Arrange
+            const String value = "";
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrEmptyOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrEmptyOrWhiteSpace_ReturnsFalse_ForFilledString()
+        {
+            // Arrange
+            const String value = "SomeValue";
+            const Boolean expectedValue = false;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrEmptyOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void Test_IsNullOrEmptyOrWhiteSpace_ReturnsFalse_ForWhiteSpace()
+        {
+            // Arrange
+            const String value = "    ";
+            const Boolean expectedValue = true;
+
+            // Act
+
+            //Assert
+            Assert.AreEqual(expectedValue, value.IsNullOrEmptyOrWhiteSpace());
+        }
+
+        #endregion
+    }
+}
---
+++ Dibware.System.Extensions/System/String.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Dibware.System.Extensions
+{
+    /// <summary>
+    /// Extend methods for the System.String object
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Indicates if the instance has a value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Returns <c>true</c> if this instance has a value, otherwise <c>false</c>.</returns>
+        public static Boolean HasValue(this String value)
+        {
+            return !String.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
+        /// Indicates if the instance is null or empty.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Returns <c>true</c> if this instance is null or empty, otherwise <c>false</c>.</returns>
+        public static Boolean IsNullOrEmpty(this String value)
+        {
+            return String.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
+        /// Indicates if the instance is null or white space.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Returns <c>true</c> if this instance is null or white space, otherwise <c>false</c>.</returns>
+        public static Boolean IsNullOrWhiteSpace(this String value)
+        {
+            return String.IsNullOrWhiteSpace(value);
+        }
+
+        /// <summary>
+        /// Indicates if the instance is null, empty or white space.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Returns <c>true</c> if this instance is null, empty or white space, otherwise <c>false</c>.</returns>
+        public static Boolean IsNullOrEmptyOrWhiteSpace(this String value)
+        {
+            return String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value);
+        }
+    }
+}
