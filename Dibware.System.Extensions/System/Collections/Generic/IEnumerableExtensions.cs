7aec87d87713af640b7b7bad52af4dfd89e07d5f
dibley1973 <dibley1973@yahoo.co.uk>
2014-04-10 21:15:16 +0100

    20140410-2115-DWLT Added IEnumerableExtensions

---
+++ Dibware.System.Extensions/System/Collections/Generic/IEnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dibware.System.Extensions.System.Collections.Generic
+{
+    /// <summary>
+    /// Provides extensionn methods to the IEnumerable class
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="action"></param>
+        public static void ForEach<T>(this IEnumerable<T> source,
+            Action<T> action)
+        {
+            foreach (T element in source)
+            {
+                action(element);
+            }
+        }
+    }
+}
