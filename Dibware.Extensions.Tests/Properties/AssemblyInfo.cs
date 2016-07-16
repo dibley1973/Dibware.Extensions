fb5914751863803f5c19f9340d6eee8e5fb5a22c
dibley1973 <dibley1973@yahoo.co.uk>
2016-07-16 10:13:33 +0100

    20160716_1013_DWDT Updated code versions, as forgot at last commit

--- Dibware.Extensions.Tests/Properties/AssemblyInfo.cs
+++ Dibware.Extensions.Tests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dibware.Extensions.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit test project for Dibware Extensions library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dibware")]
 [assembly: AssemblyProduct("Dibware.Extensions.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Dibware")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
